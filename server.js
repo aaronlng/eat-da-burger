e02627bc18d3442f5e2a6f3cf506afea2cf49cf2
Aaron Long <aaronlng05@gmail.com>
2019-09-10 14:04:52 -0400

    set up directory and server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+// Serve static content for the app from the "public" folder
+app.use(express.static("public"));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function () {
+    console.log("Listening on port:%s", PORT);
+});
