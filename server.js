89a07a7aa0a5e44663d8f79b1a91eda408bd5dce
Aaron Long <aaronlng05@gmail.com>
2019-09-12 08:29:33 -0400

    update server with router

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
 app.listen(PORT, function () {
     console.log("Listening on port:%s", PORT);
 });
