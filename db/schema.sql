6aa3380d5d943a9c6c49fdca2ed84a4b628e2908
Aaron Long <aaronlng05@gmail.com>
2019-09-13 14:17:50 -0400

    update schema for jaws db

--- db/schema.sql
+++ db/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOL DEFAULT false,
-    createdAt TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
