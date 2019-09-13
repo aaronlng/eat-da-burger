9c75a7d7815c4ff02cc45df4965e910537ddb12d
Aaron Long <aaronlng05@gmail.com>
2019-09-13 14:14:39 -0400

    update schema for Jaws DB

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,21 @@ CREATE TABLE burgers (
     devoured BOOL DEFAULT false,
     PRIMARY KEY (id)
 );
+
+/* JAWS DB SCHEMA */
+
+/* 
+
+USE acahov4xh2vgt28p;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
+
+*/
